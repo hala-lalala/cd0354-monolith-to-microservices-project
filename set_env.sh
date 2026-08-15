ae15982881135ea1c075fec768ab845d2b82d4e1
vivien.hora <vivien.hora@accenture.com>
2026-08-15 11:34:39 +0800

    converted to microservice

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=xxxxxxx
+export POSTGRES_HOST=db-lesson.chnl8hgbyzva.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=mybucket1200798
+export AWS_BUCKET=test-4949207-dev
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=lesson-4-profile
 export JWT_SECRET=testing
 export URL=http://localhost:8100
